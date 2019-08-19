27470fed1048f0ac628e0c487873d14eea8b9fd1
Silvio <silvio0407@gmail.com>
2019-08-19 14:52:40 +0100

    redirecionamento pagina load

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,16 +1,27 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { Observable } from 'rxjs';
 
 
 @Injectable()
 export class AuthGuard implements CanActivate{
 
-    constructor(private router: Router){}
+    constructor(
+        private router: Router,
+        private authService: AuthService
+        ){}
     
     // not logged in so redirect to login page with the return url
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+   /* canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         // not logged in so redirect to login page with the return url
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
         return false;
-    }
+    }*/
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        return this.authService.isLoggedIn();
+      }
 }
